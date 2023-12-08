c3f6903c41b3cae7880d65e1e018aa0b994447ad
divinged@inbox.ru <divinged@inbox.ru>
2023-12-08 11:10:22 +0300

    Добавили решение 3 задачи

---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Задайте произвольную строку. Выясните, является ли она палиндромом.
+bool IsPalindrome(string str){
+    string normalized = new string(str.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+    System.Console.WriteLine($"Строка после удаления небуквенно-цифровых символов(например пробелов)  {normalized}");
+    return normalized.SequenceEqual(normalized.Reverse());
+
+}
+
+
+string input = "ша1л1 аш";
+System.Console.WriteLine($"Начальная строка   {input}");
+
+bool isPalindrome = IsPalindrome(input);
+System.Console.WriteLine(isPalindrome);
+
